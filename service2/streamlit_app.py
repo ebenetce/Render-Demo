adaa7ced30e471155850885e1140bd9e77b7f68a
Edu Benet Cerda <ebenetce@mathworks.com>
2025-07-29 22:32:47 +0100

    streamlit for mps

--- service2/streamlit_app.py
+++ service2/streamlit_app.py
@@ -8,7 +8,7 @@ st.title("Streamlit App Connecting to Service 1")
 
 if st.button("Get Data from Service 1"):
     try:
-        response = requests.get(f"{SERVICE1_URL}/your-endpoint")
+        response = requests.post(f"{SERVICE1_URL}/mymagic/mymagic", json={"rhs":3, "nargout":1})
         st.write("Response from Service 1:", response.json())
     except Exception as e:
         st.error(f"Error connecting to Service 1: {e}")
